87f8790781ef73d5cecceb7fa8b3623847d68fd7
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-11-13 17:44:23 +0000

    Add Reactive forms

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  imports: [ReactiveFormsModule],
   standalone: true,
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = new FormGroup({
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
